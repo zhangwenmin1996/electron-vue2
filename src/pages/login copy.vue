c995e4675af503204ac7c31ae54232a19be7cb23
zwm <zhangwenmin0525@163.com>
2024-08-27 11:08:34 +0800

    1111

--- src/pages/login copy.vue
+++ src/pages/login copy.vue
@@ -1,8 +1,86 @@
 <template>
-  <div class="box">
-    <div class="noLogin" @click="offLine">
-      开始使用
+<div class="box">
+  <div class="login">
+    <!-- <div class="el-login-header">
+      <img class="login-logo" src="@/assets/logo/logo3.png" alt="" />
+    </div> -->
+    <div class="el-login-header">
+      <!-- <img class="login-logo" :src="logo" alt="" />
+      <div class="line"></div> -->
+      <h3 class="title">智能运维系统</h3>
+      <div class="dot"></div>
     </div>
+    <!-- <div class="left"></div> -->
+    <el-form
+      ref="loginForm"
+      :model="loginForm"
+      :rules="loginRules"
+      class="login-form"
+    >
+      <!-- <img class="vector1" src="@/assets/images/V1.png" alt="" />
+      <img class="vector2" src="@/assets/images/V2.png" alt="" />
+      <img class="vector3" src="@/assets/images/V3.png" alt="" />
+      <img class="vector4" src="@/assets/images/V4.png" alt="" /> -->
+      <div class="top-title">登录</div>
+      
+      <el-form-item prop="username">
+        <el-input
+          v-model="loginForm.username"
+          type="text"
+          auto-complete="off"
+          placeholder="账号"
+        >
+          <i slot="prefix" class="el-icon-user-solid el-input__icon input-icon" />
+          <!-- <svg-icon slot="prefix" icon-class="user" class="iconfont-user el-input__icon input-icon" /> -->
+        </el-input>
+      </el-form-item>
+      <el-form-item prop="password">
+        <el-input
+          v-model="loginForm.password"
+          type="text"
+          auto-complete="off"
+          placeholder="密码"
+          @keyup.enter.native="handleLogin"
+        >
+          <i
+            slot="prefix"
+            class="el-icon-lock el-input__icon input-icon"
+          />
+          <!-- <svg-icon slot="prefix" icon-class="password" class="el-input__icon input-icon" /> -->
+        </el-input>
+      </el-form-item>
+      
+      <!-- <el-checkbox
+        v-model="loginForm.rememberMe"
+        style="margin: 0px 0px 25px 0px"
+        >记住密码</el-checkbox
+      > -->
+      <el-form-item style="width: 100%">
+        <el-button
+          :loading="loading"
+          size="medium"
+          type="primary"
+          class="form-btn"
+          @click.native.prevent="handleLogin"
+        >
+          <span v-if="!loading">登 录</span>
+          <span v-else>登 录 中...</span>
+        </el-button>
+      </el-form-item>
+      <div class="form-footer">
+        <div class="setLine" @click="setLine">
+          设置网络
+        </div>
+        <div class="noLogin" @click="offLine">
+          离线使用
+        </div>
+        
+      </div>
+      
+    </el-form>
+    
+    </div>
+    <config ref="configInfo" ></config>
   </div>
 </template>
 
@@ -291,35 +369,213 @@ export default {
   @return $px / 1080 * 100vh;
 }
 .box{
-  position: relative;
   width: 100vw;
   height: 100vh;
   background: url("../assets/login/bg.png") center ;
   background-size: 100%;
   overflow: hidden;
-  .noLogin{
+  #particles-js {
     position: absolute;
+    width: 100%;
+    height: 99%;
+    padding: 0;
+    margin: 0;
+    top: 0;
+    // bottom: 0;
+    left: 0;
+    // right: 0;
+  }
+}
+.login {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  background: rgba(#000,0.4);
+  background-size: cover;
+  user-select: none;
+}
+.el-login-header {
+  position: absolute;
+  top: Hx(90);
+  // width: 100%;
+  display: flex;
+  margin: 0px auto 30px auto;
+  // overflow: hidden;
+  align-items: center;
+  .login-logo{
+    // width: 150px;
+    height: Hx(60);
+  }
+  .line{
+    height: Hx(40);
+    width: Px(2);
+    margin: Hx(10) 0 0  Hx(10);
+    background: #fff;
+  }
+  .title{
+    font-size: Hx(40);
+    font-family: 'SimHei';
+    color: #fff;
+    // font-weight: 600;
+    margin: Hx(10) 0 0 10px;
+  }
+  .dot{
+    position: absolute;
+    width: Px(1020);
+    height: Px(18);
+    bottom: Hx(-20);
     left: 50%;
-    top: 60%;
-    transform: translate(-50%,-50%);
-    width: 150px;
-    height: 40px;
+    transform: translateX(-50%);
+    background:  url("../assets/login/line.png") center no-repeat;
+    background-size: 100%;
+  }
+}
+.left{
+  width: Px(463);
+  height: Px(512);
+  background: url("../assets/login/left.png") center no-repeat;
+  background-size: 100% 100%;
+}
+.login-form {
+  position: relative;
+  width: Px(545);
+  opacity: 1;
+  padding: Hx(118) 40px Hx(40);
+  background: url("../assets/login/box.png") center no-repeat;
+  background-size: 100% 100%;
+  border-radius: Px(16);
+  .form-footer{
+    display: flex;
+    justify-content: space-between;
+     .setLine{
+      color: #90dd96;
+      &:hover{
+        color: #01C19A;
+        cursor: pointer;
+      }
+    }
+    .noLogin{
+      color: #1890ff;
+      &:hover{
+        color: #01C19A;
+        cursor: pointer;
+      }
+    }
+  }
+  
+  .header-title{
+    position: absolute;
+    top: -80px;
+    left: 50%;
+    width: 500px;
     text-align: center;
-    line-height: 40px;
-    background: #5B9BD5;
-    border: 1px #2F67A1 solid;
-    box-shadow: 0px 2px 6px rgba(208, 243, 254, 0.6);
-    border-radius: 8px;
-    &:hover{
-      background: linear-gradient(180deg, rgba(47, 103, 161, 0.4) 0%, #2F67A1 100%);
-      color: #01C19A;
+    transform: translateX(-50%);
+    color: #f00;
+    font-size: 36px;
+    font-weight: 600;
+    letter-spacing: 4.5px;
+  }
+//  &:after {
+//       content: "";
+//       width: 100%;
+//       height: 100%;
+//       position: absolute;
+//       left: 0;
+//       top: 0;
+//       /* 从父元素继承 background 属性的设置 */
+//       background: inherit;
+//       filter: blur(2px);
+//       z-index: 2;
+//   }
+  >>>.el-form-item{
+    margin-bottom: Px(22);
+  }
+  .el-input {
+    height: Hx(48) !important;
+    >>>.el-input__inner {
+      height: Hx(48) !important;
+      font-size: Hx(16);
+      color: #fff;
+      border-color: #87C1FF;
+      background: rgba(#fff,0.1)
+    }
+  }
+  >>>.el-checkbox__input.is-checked .el-checkbox__inner {
+    background-color: #87C1FF;
+    border-color: #87C1FF;
+  }
+  >>>.el-checkbox__label{
+    color: #87C1FF;
+  }
+  .input-icon {
+    width: 30px;
+    height: Hx(45);
+    line-height: Hx(45);
+    color: #87C1FF;
+    margin: 0 2px;
+  }
+  .vector1,.vector2,.vector3,.vector4{
+    position: absolute;
+    width: 67px;
+  }
+  .vector1{
+    top: 0;
+    left: 0;
+  }
+  .vector2{
+    top: 0;
+    right: 0;
+  }
+  .vector3{
+    bottom: 0;
+    right: 0;
+  }
+  .vector4{
+    bottom: 0;
+    left: 0;
+  }
+}
+.top-title {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  text-align: center;
+  font-size: Px(24);
+  font-family: 'SimHei';
+  color: #fff;
+  font-weight: 600;
+  margin: Hx(10) 0 0 0;
+}
+
+.form-btn {
+  width: 100%;
+  height: Px(50);
+  opacity: 1;
+  border: 0;
+  margin-top: Px(25);
+  background: linear-gradient(180deg, rgba(47, 103, 161, 0.4) 0%, #2F67A1 100%);
+  box-shadow: 0px 2px 6px rgba(208, 243, 254, 0.6);
+  border-radius: 8px;
+
+}
+.footer{
+  position: absolute;
+  bottom: 20px;
+  width: 100%;
+  color: #ccc;
+  text-align: center;
+  .beian{
+    display: inline-block;
+    margin-left: 15px;
+      &:hover{
+      color: #1890ff;
       cursor: pointer;
     }
   }
+
 }
-
-
-
 
 </style>
 
